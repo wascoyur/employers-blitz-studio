836f9a7c51aed6470cd236470bec6470062de73a
yur <mytem20007@gmail.com>
2020-12-06 12:52:53 +0500

    Card didMount img is done Card.didMount изображения загружает

--- src/services/swap-service.js
+++ src/services/swap-service.js
@@ -1,14 +1,37 @@
 const _apiBase = 'https://pokeapi.co/api/v2/';
 
-const swapData = async (id) => {
+const getData = async (id = 'pokemon/') => {
   const res = await fetch(`${_apiBase}${id}`);
+  console.log('getData fetch:', `${_apiBase}${id}`);
+
+  if (!res.ok) {
+    throw new Error('Что-то пошло не так:', res.statusText);
+  }
+  const out = await res.json();
+  return await out;
+};
+
+const getFullUrl = async (urlOrId) => {
+  const res = await fetch(`url`);
+  console.log('getFullUrl:', `url`);
+
   if (!res.ok) {
     throw new Error('Что-то пошло не так');
   }
-  const o = await res.json();
-  console.log('o', o);
+  const out = await res.json();
+  console.log('getFullUrl:', out);
 
-  return await o;
+  return await out;
 };
+const testFunc = (id)=>{console.log('test', id);
+}
+const getPokeMain = async (nameOrId) => {
+  //nameOrId = 'pokemon/2/';
+  const pointer = `${nameOrId}`;
+  //console.log('getPokeMain-pointer', pointer);
 
-export default swapData;
+  const pokeMain = await getData(`pokemon/${pointer}`);
+  console.log('getPokeMain-pokeMain', await pokeMain);
+  return pokeMain;
+};
+export { getData, getPokeMain };
